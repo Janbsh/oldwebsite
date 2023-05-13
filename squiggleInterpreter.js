66b063ef63b9829f2607607b14997c6dc9b7e3bb
Ethan <janbshemail@gmail.com>
2023-05-13 03:30:17 -0500

    added squiggle + a ton of other stuff

---
+++ squiggleInterpreter.js
@@ -0,0 +1,47 @@
+function interpretSquiggle(code) {
+    const validChars = "~+-/*";
+    const error = "Error";
+    let tb = 0;
+    let ob = 0;
+    let op;
+    for(const c of code){
+        if(c === '~') tb ++;
+        else if("+-/*".indexOf(c) > -1) {
+            if(op === undefined) ob = tb;
+            else if(op === "+") ob += tb
+            else if(op === "-") ob -= tb
+            else if(op === "*") ob *= tb
+            else if(op === "/") ob /= tb
+            tb = 0; 
+            op = c;
+        }
+    }
+    if(op === "+") ob += tb
+    else if(op === "-") ob -= tb
+    else if(op === "*") ob *= tb
+    else if(op === "/") ob /= tb
+    else ob = tb;
+    if (!code.split('').every(c => validChars.includes(c))) return error;
+    else return ob;
+}
+
+var runButton;
+var editor;
+var console;
+
+addEventListener("load", () => {
+    runButton = document.getElementById("run-button");
+    editor = document.getElementById("editor");
+    console = document.getElementById("console");
+});
+
+function run() {
+    const code = editor.value;
+    let result = interpretSquiggle(code);
+    console.value = result;
+}
+
+function clearAll() {
+    editor.value = "";
+    console.value = "";
+}
